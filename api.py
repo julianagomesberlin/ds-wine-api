a2675cb451b8e50f26e839e881064111df48e3d8
Juliana <jujunerdiceseafins@gmail.com>
2021-06-23 22:14:38 +0200

    Implement search by multiple criteria

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import json
 import flask
 from flask import jsonify
+from flask import request
 
 # read and parse
 with open('wine-detailed-data.json', 'r') as json_file:
@@ -60,7 +61,7 @@ def home():
     return "HEY"
 
 @app.route('/wine/all', methods=['GET'])
-def print_all():
+def wine_all():
     return jsonify(all_wines)
 
 @app.route('/wine/foods', methods=['GET'])
@@ -74,6 +75,22 @@ def wine_countries():
 @app.route('/wine/grapes', methods=['GET'])
 def wine_grapes():
     return jsonify(grape_names)
+
+@app.route('/wine/search', methods=['GET'])
+def wine_search():
+    food_name = request.args["food_name"] if "food_name" in request.args else False
+    country_name = request.args["country_name"] if "country_name" in request.args else False
+    grape_name = request.args["grape_name"] if "grape_name" in request.args else False
+    price_min = float(request.args["price_min"]) if "price_min" in request.args else False
+    price_max = float(request.args["price_max"]) if "price_max" in request.args else False
+    has_price = price_min and price_max
+
+    wines_with_food_name = list(filter(lambda wine: wine['food_names'] != None and food_name in wine['food_names'], all_wines)) if food_name else all_wines
+    wines_with_country_name = list(filter(lambda wine: wine['country_name'] != None and country_name == wine['country_name'], wines_with_food_name)) if country_name else wines_with_food_name
+    wines_with_grape_name = list(filter(lambda wine: wine['grape_names'] != None and grape_name in wine['grape_names'], wines_with_country_name)) if grape_name else wines_with_country_name
+    wines_with_price = list(filter(lambda wine: wine['price_eur'] != None and wine['price_eur'] >= price_min and wine['price_eur'] <= price_max, wines_with_grape_name)) if has_price else wines_with_grape_name
+
+    return jsonify(wines_with_price)
 
 # dev mode
 app.run() 
